94383570828577ffe03cffee52624b6f07a256b2
Scott Eisele <scott.r.eisele@vanderbilt.edu>
2018-03-26 18:17:32 -0500

    remove unused files

--- code/PhaseII/components/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-scp *.py blockchain:~/components/
-scp ../data/*.csv blockchain:~/data/
-ssh blockchain 'scp components/*.py eth1:~/components/'
-ssh blockchain 'scp data/*.csv eth1:~/data/'
-
--- code/PhaseII/launch-prosumers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR=$(pwd)
-echo $DIR
-
-rm -rf $DIR/test-10-11-withbattery/prosumer*
-$DIR/test-10-11-withbattery/testrun.sh
