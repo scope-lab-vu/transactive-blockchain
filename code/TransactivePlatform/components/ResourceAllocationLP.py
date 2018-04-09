f8eb270ff98a52434160df33be5b88bd3e47a96d
Scott Eisele <scott.r.eisele@vanderbilt.edu>
2018-04-09 17:57:52 -0500

    fix type, c to co

--- code/TransactivePlatform/components/ResourceAllocationLP.py
+++ code/TransactivePlatform/components/ResourceAllocationLP.py
@@ -1,22 +1,22 @@
 import logging
 
-from LinearProgramCplex import LinearProgramCplex 
+from LinearProgramCplex import LinearProgramCplex
 
 class Offer:
   def __init__(self, ID, providing, prosumer, quantity={}, value={}):
     self.ID = ID
     self.providing = providing
-    self.prosumer = prosumer 
+    self.prosumer = prosumer
     self.quantity = quantity
     self.value = value
-    
+
   def matchable(self, offer):
     for res_type in self.quantity:
       if res_type in offer.quantity:
         if (self.providing and (not offer.providing) and (self.value[res_type] <= offer.value[res_type])) or ((not self.providing) and offer.providing and (self.value[res_type] >= offer.value[res_type])):
           return True
     return False
-    
+
   def intersection(self, offer):
     res_types = []
     for res_type in self.quantity:
@@ -28,7 +28,7 @@ class Offer:
 class ResourceAllocationLP:
   def __init__(self, precision):
     self.precision = precision
-    
+
   def solve(self, providing_offers, consuming_offers, lp_solver=LinearProgramCplex):
     program = lp_solver()
     variables = {}
@@ -38,7 +38,7 @@ class ResourceAllocationLP:
       for co in consuming_offers:
         for t in po.intersection(co):
           variable = {'po': po, 'co': co, 't': t}
-          varname = 'q_{}_{}_{}'.format(po.ID, c.ID, t)
+          varname = 'q_{}_{}_{}'.format(po.ID, co.ID, t)
           variables[varname] = variable
           if po in prov_vars:
             prov_vars[po][varname] = po.quantity[t]
@@ -51,7 +51,7 @@ class ResourceAllocationLP:
     if not len(variables):
       logging.info("No matchable offers, skipping solver.")
       return ([], 0)
-            
+
     # TODO: add integer constraints based on precision
     program.set_objective({varname: -1.0 for varname in variables})
     for po in prov_vars:
@@ -60,7 +60,7 @@ class ResourceAllocationLP:
     for co in cons_vars:
       program.add_constraint(
         {varname: 1.0 / float(cons_vars[co][varname]) for varname in cons_vars[co]}, 1.0)
-          
+
     (solution, objective) = program.solve()
     for varname in variables:
       variables[varname]['q'] = solution[varname]
@@ -70,4 +70,3 @@ if __name__ == "__main__":
   solver = ResourceAllocationLP(1)
   (assignments, objective) = solver.solve()
   print("Success: objective = {}".format(objective))
-
