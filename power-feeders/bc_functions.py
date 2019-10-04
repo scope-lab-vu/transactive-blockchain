cea02fce08050df532513e66fe8a443f33fbe19b
Carlos Barreto <carlos.a.barreto@vanderbilt.edu>
2019-10-03 19:40:05 -0500

    added functions to send bids through the block chain

---
+++ power-feeders/bc_functions.py
@@ -0,0 +1,172 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+import pdb
+from datetime import datetime, date, time, timedelta
+import copy
+import random 
+import os
+
+from functions_auction import build_curves, find_equilibrium_auction, extract_bids, order_bids_descending, order_bids_ascending, find_market_equilibrium, surplus_f, ideal_eq_attack, maer, avg_total_surplus, stats
+
+from functions_read_data import read_data, rolling_statistics, get_avg_data
+
+from core.EthereumClient import EthereumClient
+from core.MatchingContract import MatchingContract
+from core.MatchingSolver import MatchingSolver, Offer
+import config as cfg
+import time
+
+
+ethclient = EthereumClient(ip='localhost', port=10000, TXGAS=cfg.TRANSACTION_GAS)
+
+account = ethclient.accounts()[0] # use the first owned address
+
+code = '1234321'
+
+def encode(price, quantity):
+	bid_price = int(float(price)*1000)
+	bid_quantity = int(abs(float(quantity)*1000))
+	return str(bid_price) + code + str(bid_quantity)
+
+def decode(n1):
+	parameters = str(n1).split(code)
+	bid_price = float(parameters[0])/1000
+	bid_quantity = float(parameters[1])/1000
+	return bid_price, bid_quantity
+
+
+
+
+
+# send the bids to the block chain
+def post(parameters):
+	bidder_name, price, quantity, period, time = parameters
+
+	# write the bids in logs
+	data = [period, time, bidder_name, price, quantity, 'unknown']
+	f = open('bids.csv', 'a')
+	f.write( ','.join(data) + '\n' )
+	f.close()
+
+	f = open('bids_log.csv', 'a')
+	f.write( ','.join(data) + '\n' )
+	f.close()
+
+
+	# get the contract address
+	with open('contract_address', 'r') as f:
+		contract_address = f.readline()
+
+	# define the contract
+	contract = MatchingContract(ethclient, contract_address)
+
+	# get the current period
+	for event in contract.poll_events():
+	    name = event['name']
+	    if (name == "StartOffering"):
+		nextInterval = params['interval']
+
+	start_time = nextInterval
+	end_time = start_time+1
+
+	# send the bids
+	bid_quantity = float(quantity)
+
+	energy = encode(price, quantity)
+
+	if bid_quantity < 0:
+		txHash = contract.postBuyingOffer(account, bidder_name, start_time, end_time, energy)
+		receipt = wait4receipt(ethclient, txHash, "postBuyingOffer")
+	else:
+		txHash = contract.postSellingOffer(account, bidder_name, start_time, end_time, energy)
+		receipt = wait4receipt(ethclient, txHash, "postSellingOffer")
+
+	return '1'
+
+
+
+
+def get_solution(parameters):
+
+	# get the contract address
+	with open('contract_address', 'r') as f:
+		contract_address = f.readline()
+
+	# define the contract
+	contract = MatchingContract(ethclient, contract_address)
+	
+	# get the bids
+	bids = dict()
+	bids_demand = []
+	bids_offer = []
+
+	for event in contract.poll_events():
+	    params = event['params']
+	    name = event['name']
+
+	    if (name == "BuyingOfferPosted") or (name == "SellingOfferPosted"):
+		new_offers = True
+
+		energy = params['energy']
+		p, q = decode(energy)
+		bidder = params['prosumer'] 
+
+		bids[bidder] = [p, q]
+
+		if name == "BuyingOfferPosted":
+			bids_demand.append( [p, q] )
+		else:
+			bids_demand.append( [p, q] )
+
+
+		# transform into an array
+		bids_demand = np.array(bids_demand)
+		bids_offer = np.array(bids_offer)
+
+		# order the bids
+		bids_demand = order_bids_descending(bids_demand)
+		bids_offer = order_bids_ascending(bids_offer)
+
+		# find the equilibria
+		number_offers = len(bids_offer)
+		number_demand = len(bids_demand)
+
+		if number_demand<=0 or number_offers<=0:
+			q_eq = 0
+			p_eq = 0
+
+		else:
+			q_eq, p_eq = find_equilibrium_auction(bids_offer, bids_demand)
+
+	
+
+	# get the equilibrium and the bids in each time period
+	file_name = 'bids.csv'
+	try:
+		eq_time_nom, market_eq_nom, bids_nom, curves_nom = find_market_equilibrium(file_name)
+	except:
+		print('error')
+		pdb.set_trace()
+
+	# rewrite the bid file
+	try:
+		os.remove(file_name)
+	except:
+		pass
+
+	# initialize the file for the bids of the next period
+	f = open(file_name, 'w')
+	f.write( 'market_id,timestamp,bidder_name,bid_price,bid_quantity,bid_state\n' )
+	f.close()
+
+
+	# write the equilibria price
+	prices = [str(market_eq_nom['p'][0]), str(p_eq)]
+	f = open('eq_price.csv', 'a')
+	f.write( ','.join(prices) + '\n' )
+	f.close()
+
+	#return str(market_eq_nom['p'][0])
+	return str(p_eq)
+	
---
+++ power-feeders/register_bidders.py
@@ -0,0 +1,93 @@
+from core.EthereumClient import EthereumClient
+from core.MatchingContract import MatchingContract
+from core.MatchingSolver import MatchingSolver, Offer
+import config as cfg
+import time
+
+
+ethclient = EthereumClient(ip='localhost', port=10000, TXGAS=cfg.TRANSACTION_GAS)
+
+account = ethclient.accounts()[0] # use the first owned address
+
+
+def wait4receipt(ethclient,txHash,name,getReceipt=True):
+
+    if not getReceipt:
+        receipt = {}
+        receipt['gasUsed'] = -1
+        receipt['cumulativeGasUsed'] = -1
+        print("Did not wait for receipt")
+        return receipt
+
+    if txHash.startswith("0x"): 
+
+        receipt = ethclient.command("eth_getTransactionReceipt", params=[txHash])       
+        while receipt is None or "ERROR" in receipt:
+            
+            print("Waiting for tx to be mined... (block number: {})".format(ethclient.command("eth_blockNumber", params=[])))
+            time.sleep(5) 
+
+            receipt = ethclient.command("eth_getTransactionReceipt", params=[txHash])
+
+        if receipt['gasUsed'] == cfg.TRANSACTION_GAS:
+            print("Transaction may have failed. gasUsed = gasLimit")
+
+        print("%s gasUsed: %s" %(name,receipt['gasUsed']))
+        print("%s cumulativeGasUsed: %s" %(name,receipt['cumulativeGasUsed']))
+
+        return receipt
+
+
+def deploy_contract(BYTECODE, TXGAS):
+    print("Deploying contract...")
+    # use command function because we need to get the contract address later
+    txHash = ethclient.command("eth_sendTransaction", params=[{'data': BYTECODE, 'from': account, 'gas': TXGAS}])
+    print("Transaction hash: " + txHash)
+
+    receipt = wait4receipt(ethclient, txHash, "deployContract")
+
+    contract_address = receipt['contractAddress']
+
+    return contract_address
+
+
+contract = None
+contractBYTECODE = '/home/riaps/projects/transactive-blockchain/transax/smartcontract/output/MatchingContract.bin'
+with open(contractBYTECODE) as f:
+    BYTECODE = "0x"+f.read()
+    contract_address = deploy_contract(BYTECODE, cfg.TRANSACTION_GAS)
+    contract = MatchingContract(ethclient, contract_address)
+
+contract.setup(account, cfg.MICROGRID.C_ext, cfg.MICROGRID.C_int, cfg.START_INTERVAL)
+print("Contract address: " + contract_address)
+
+
+
+#################################################################
+# load the list of bidders
+file_bidders = './power-feeders/bidders'
+list_bidders = []
+with open(file_bidders, 'r') as f_bidders:
+	for x in f_bidders:
+		list_bidders.append(x)
+
+
+# register prosumers
+for prosumer_id in list_bidders:
+	txHash = contract.registerProsumer(account, prosumer_id, cfg.PROSUMER_FEEDER[prosumer_id])
+	receipt = wait4receipt(ethclient, txHash, "registerProsumer")
+
+
+# write the address of the contract
+file_name = 'contract_address'
+try:
+	os.remove(file_name)
+except:
+	pass
+
+f = open(file_name, 'w')
+f.write( contract_address )
+f.close()
+
+
+
