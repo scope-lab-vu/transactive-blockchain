ab7c53a5de1cddb52be068de1bb09eaeafe07780
Scott <scott.r.eisele@vanderbilt.edu>
2020-07-16 17:35:04 +0000

    change axis scale from 0-1 to 0-100

--- power-feeders/attack_design/plot_impact_mitigation.py
+++ power-feeders/attack_design/plot_impact_mitigation.py
@@ -6,21 +6,28 @@ from datetime import datetime, date, time, timedelta
 import matplotlib.dates as mdates
 
 
-case = 'attack_1'
+# case = 'attack_1'
 case = 'attack_2'
 
-stats_exp = np.load('stats_'+case+'.npy')
+stats_exp = np.load('stats_'+case+'.npy', allow_pickle=True)
 
-mean_profit = [ exp['avg'] for exp in stats_exp ]
-std_profit = [ exp['std'] for exp in stats_exp ]
+mean_profit = [ exp['avg']*100 for exp in stats_exp ]
+std_profit = [ exp['std']*100 for exp in stats_exp ]
 
 # mitigation
-x = [0, 0.2, 0.4, 0.6, 0.8]
+x = [0, 20, 40, 60, 80]
+
+
 
 plt.figure(9)
 plt.clf()
 plt.errorbar(x, mean_profit, std_profit, linestyle='None', marker='s')
+plt.xlabel('Mitigation Rate [%]')
+plt.ylabel('Profit [%]') 
+plt.ylim(-15,40)
 plt.show()
 
 
+
+plt.savefig('%s.pgf'%case, bbox_inches='tight')
 
